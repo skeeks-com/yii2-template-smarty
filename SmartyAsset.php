75a7e5376effbc40284500562b298af968905300
Semenov <semenov@skeeks.com>
2015-09-18 11:17:12 +0300

    Скачал залил шаблон

---
+++ SmartyAsset.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (СкикС)
+ * @date 18.09.2015
+ */
+namespace skeeks\template\smarty;
+
+use yii\web\AssetBundle;
+
+class SmartyAsset extends AssetBundle
+{
+    public $sourcePath = '@skeeks/template/smarty/src/HTML/assets/';
+}
